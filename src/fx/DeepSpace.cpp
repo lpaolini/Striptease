9317bca70a3ddff07eef90a7a065f64c6e5d4574
Luca Paolini <lookap@gmail.com>
2021-01-16 01:20:45 +0100

    Implement DeepSpace effect direction changes.

--- src/fx/DeepSpace.cpp
+++ src/fx/DeepSpace.cpp
@@ -15,6 +15,9 @@ void DeepSpace::reset() {
         randomizeItem(items[i]);
     }
     time = 0;
+    steeringAngle = 0;
+    transition = 1;
+    timer.reset();
 }
 
 void DeepSpace::loop() {
@@ -22,56 +25,100 @@ void DeepSpace::loop() {
 
     float dT = time / 1e6;
     time = 0;
+
     bool trigger = audioChannel->signalDetected
         ? audioChannel->beatDetected
         : random8() == 0;
 
+    float rotationAngle = 0;
+
+    if (transition == 1) {
+        if (timer.isElapsed()) {
+            switch (random(5)) {
+                case 1:
+                    steeringAngle = -random(MIN_STEERING, MAX_STEERING);
+                    transitionSpeed = -.001 * random(1, 5) * steeringAngle;
+                    transition = 0;
+                    break;
+                case 2:
+                    steeringAngle = random(MIN_STEERING, MAX_STEERING);
+                    transitionSpeed = .001 * random(1, 5) * steeringAngle;
+                    transition = 0;
+                    break;
+                default:
+                    steeringAngle = 0;
+                    transitionSpeed = 0;
+                    break;
+            }
+            timer.reset();
+        }
+    } else {
+        float previousTransition = transition;
+        transition = min(1, transition + transitionSpeed * dT);
+        rotationAngle = steeringAngle * deltaEaseInOutSine(transition, previousTransition) / 180 * TWO_PI;
+    }
+
     for (uint8_t i = 0; i < ITEMS; i++) {
+        moveItem(items[i], speed * dT);
+        rotateItem(items[i], rotationAngle);
         loopItem(items[i], dT, trigger);
     }
 }
 
+float DeepSpace::deltaEaseInOutSine(float x1, float x0) {
+    return (cos(PI * x0) - cos(PI * x1)) / 2;
+}
+
+void DeepSpace::rotateItem(Item &item, float theta) {
+    float cosTheta = cos(theta);
+    float sinTheta = sin(theta);
+    item.x = item.x * cosTheta - item.y * sinTheta;
+    item.y = item.x * sinTheta + item.y * cosTheta;
+}
+
+void DeepSpace::moveItem(Item &item, float distance) {
+    item.y -= distance;
+}
+
 void DeepSpace::loopItem(Item &item, float dT, bool &trigger) {
-    item.y += v * dT;
     if (item.y < 0) {
         randomizeItem(item);
     } 
-    if (item.visible) {
-        float angle = atan2f(item.y, item.x);
-        float distanceSquared = powf(item.x, 2) + powf(item.y, 2);
-        float pos = 1 - (angle / HALF_PI);
-        uint8_t brightness = min(255, 10 * MAX_SQUARE_DISTANCE / distanceSquared);
 
-        // if (trigger && item.type == BLUE && item.y < (MAX_Y_DISTANCE + MIN_Y_DISTANCE) / 2) {
-        if (trigger && item.type == BLUE && item.y < MAX_MUTATION_Y_DISTANCE && item.x < MAX_MUTATION_X_DISTANCE) {
-            trigger = false;
-            item.type = RED;
-        }
+    float angle = atan2f(item.y, item.x);
+    float distanceSquared = powf(item.x, 2) + powf(item.y, 2);
+    float distance = sqrt(distanceSquared);
+    float pos = min(max(0, 1 - (angle / PI)), 1);
+    uint8_t brightness = min(255, 5 * MAX_SQUARE_DISTANCE / distanceSquared);
 
-        CRGB color;
-        switch (item.type) {
-            case BLUE:
-                color = CHSV(160, 255, brightness);
-                break;
-            case RED:
-                color = CHSV(0, 255, brightness);
-                break;
-            default:
-                color = CRGB::Black;
-                break;
-        }
-
-        item.pixel.setNormalized(pos, color);
+    if (trigger && item.type == BLUE && distance < MAX_MUTATION_DISTANCE) {
+        trigger = false;
+        item.type = RED;
     }
+
+    CRGB color;
+    switch (item.type) {
+        case BLUE:
+            color = CHSV(160, 255, brightness);
+            break;
+        case RED:
+            color = CHSV(0, 255, brightness);
+            break;
+        default:
+            color = CRGB::Black;
+            break;
+    }
+
+    item.pixel.setNormalized(pos, color);
 }
 
 void DeepSpace::randomizeItem(Item &item) {
-    item.x = random(MIN_X_DISTANCE, MAX_X_DISTANCE);
-    item.y = random(MIN_Y_DISTANCE, MAX_Y_DISTANCE);
-    item.type = random(100) < 100 * state->parabolicFxSpeed ? BLUE : HIDDEN;
-    // item.hue = 160;
-    // item.hue = random(100) == 0 ? 0 : 160;
+    long distance = random(MIN_DISTANCE, MAX_DISTANCE);
+    float angle = random(0, 18000) / (100 * PI);
+    item.x = distance * cosf(angle);
+    item.y = distance * sinf(angle);
+    item.type = random(100) < 100 * state->parabolicFxSpeed 
+        ? BLUE
+        : HIDDEN;
     item.pixel.reset();
-    // item.visible = random(100) < 100 * state->parabolicFxSpeed;
-    item.visible = true;
 }
--- src/fx/DeepSpace.h
+++ src/fx/DeepSpace.h
@@ -12,33 +12,35 @@
 
 class DeepSpace : public Fx {
     private:
-        const CRGBPalette16 PALETTE = CRGBPalette16(CRGB::Blue, CRGB::Red);
-        enum Type {HIDDEN, BLUE, RED};
+        enum Type {HIDDEN, BLUE, PULSE, RED};
         struct Item {
             float x;
             float y;
-            uint8_t hue;
             Pixel pixel;
-            bool visible;
             Type type;
-            // bool visible;
         };
         Strip *strip;
         AudioChannel *audioChannel;
         State *state;
-        static const uint8_t ITEMS = 100;
-        static const uint16_t MIN_X_DISTANCE = 10;
-        static const uint16_t MAX_X_DISTANCE = 1000;
-        static const uint16_t MIN_Y_DISTANCE = 1000;
-        static const uint16_t MAX_Y_DISTANCE = 2000;
-        static const uint16_t MAX_MUTATION_X_DISTANCE = 500;
-        static const uint16_t MAX_MUTATION_Y_DISTANCE = 500;
-        static constexpr float MAX_SQUARE_DISTANCE = powf(MAX_X_DISTANCE, 2) + powf(MAX_Y_DISTANCE, 2);
+        static const uint16_t ITEMS = 250;
         static const uint16_t SPEED = 500;
+        static constexpr float MIN_DISTANCE = 500;
+        static constexpr float MAX_DISTANCE = 2000;
+        static constexpr float MAX_MUTATION_DISTANCE = 500;
+        static constexpr float MAX_SQUARE_DISTANCE = pow(MAX_DISTANCE, 2);
+        static constexpr float MIN_STEERING = 30;
+        static constexpr float MAX_STEERING = 150;
         Item items[ITEMS];
         elapsedMicros time;
-        float v = -SPEED;
+        float speed = SPEED;
+        float steeringAngle = 0;
+        float transitionSpeed = 0;
+        float transition = 1;
+        Timer timer = Timer(1000);
         void loopItem(Item &item, float dT, bool &trigger);
+        float deltaEaseInOutSine(float x1, float x0);
+        void rotateItem(Item &item, float angle);
+        void moveItem(Item &item, float distance);
         void randomizeItem(Item &item);
 
     public:
