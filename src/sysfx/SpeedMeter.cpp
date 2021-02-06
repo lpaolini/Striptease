81759c4b43c95101a3a6024b1380bd343bfc4f08
Luca Paolini <lookap@gmail.com>
2021-02-06 18:17:47 +0100

    Update SpeedMeter background color.

--- src/sysfx/SpeedMeter.cpp
+++ src/sysfx/SpeedMeter.cpp
@@ -11,6 +11,6 @@ void SpeedMeter::reset() {
 
 void SpeedMeter::loop() {
     strip->off();
-    strip->paintNormalized(0, 1, CHSV(180, 255, 75), false);
-    strip->paintNormalizedSize(state->linearFxSpeed, 5, CHSV(0, 0, 255), false);
+    strip->paintNormalized(0, 1, CHSV(160, 255, 50), false);
+    strip->paintNormalizedSize(state->linearFxSpeed, 5, CHSV(0, 0, 128), false);
 }
