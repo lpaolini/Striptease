9935c8c75811e16798676fef97652488d0bc8cca
Luca Paolini <luca.paolini@fao.org>
2025-01-09 09:51:58 +0100

    Cleanup.

--- src/Gradient.h
+++ src/Gradient.h
@@ -31,19 +31,17 @@ class Gradient {
         }
 
         CRGB getColor(double fraction) {
-            if (colors.size() > 1) {
+            if (colors.size()) {
                 uint8_t segments = colors.size() - 1;
-                if (fraction < 1) {
-                    uint8_t segment = floor(fraction * segments);
-                    return interpolate(colors.at(segment), colors.at(segment + 1), fraction * segments - segment);                    
-                } else {
-                    return colors.at(segments);
+                if (segments > 1) {
+                    if (fraction < 1) {
+                        uint8_t segment = floor(fraction * segments);
+                        return interpolate(colors.at(segment), colors.at(segment + 1), fraction * segments - segment);                    
+                    }
                 }
-            } else if (colors.size() == 1) {
-                return colors.at(0);
-            } else {
-                return CRGB::Black;
+                return colors.at(segments);
             }
+            return CRGB::Black;
         }
 };
 
